b9d8d54d411fb9abf78afee3a4dcb864d05964ca
Administrator <Administrator@PC-20180130CRYZ>
2019-09-17 20:30:48 +0800

    增加Decorator Driver test

--- design-pattern-practise/src/main/java/org/trainning/dp/structure/decorator/DriverTest.java
+++ design-pattern-practise/src/main/java/org/trainning/dp/structure/decorator/DriverTest.java
@@ -5,6 +5,8 @@ public class DriverTest {
 	public static void main(String[] args) {
 		Decorator d = new ConcreteDecorate1(new ConcreteComponent());
 		d.sampleOperation("9000000000XXF");
+		d = new ConcreteDecorate2(new ConcreteComponent());
+		d.sampleOperation("0xfffffffffffffff");
 
 	}
 
