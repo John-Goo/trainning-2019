52dcabc4932cb440d79adb2fb141782f0c1ed841
pactera <pactera@pactera-PC.mshome.net>
2019-09-17 11:04:32 +0800

    driver test

--- design-pattern-practise/src/main/java/org/trainning/dp/structure/composite/Test.java
+++ design-pattern-practise/src/main/java/org/trainning/dp/structure/composite/Test.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Test {
 
 	public static void main(String[] args) {
+		
 		Employer pm = new ProjectManager("项目经理");
 		Employer pa = new Assistant("项目助理");
 		Employer programmer1 = new Programmer("程序员一");
@@ -13,6 +14,7 @@ public class Test {
 		pm.addEmployer(programmer2);//*项目经理*加程序员
 		pm.addEmployer(programmer1);
 		List<Employer> ems = pm.getEmployers();
+		
 		ems.forEach(employer->{
 			System.out.println(employer.getName());
 		});
